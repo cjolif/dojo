91f9063ec24b7f83172c18987338916f6c84002c
Christophe Jolif <cjolif@gmail.com>
2015-10-02 16:17:51 +0200

    fixes #18724, by making sure the code is always executed even if useful only on IE, Trident, Edge

--- dom-style.js
+++ dom-style.js
@@ -199,17 +199,16 @@ define(["./sniff", "./dom"], function(has, dom){
 	function _toStyleValue(node, type, value){
 		//TODO: should we really be doing string case conversion here? Should we cache it? Need to profile!
 		type = type.toLowerCase();
-		if(has("ie") || has("trident")){
-			if(value == "auto"){
-				if(type == "height"){ return node.offsetHeight; }
-				if(type == "width"){ return node.offsetWidth; }
-			}
-			if(type == "fontweight"){
-				switch(value){
-					case 700: return "bold";
-					case 400:
-					default: return "normal";
-				}
+		// in practice the two tests below are only useful for IE in all its forms
+		if(value == "auto"){
+			if(type == "height"){ return node.offsetHeight; }
+			if(type == "width"){ return node.offsetWidth; }
+		}
+		if(type == "fontweight"){
+			switch(value){
+				case 700: return "bold";
+				case 400:
+				default: return "normal";
 			}
 		}
 		if(!(type in _pixelNamesCache)){
